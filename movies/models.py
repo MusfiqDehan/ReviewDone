53d4c44405c752744a3746897b79cfa9a6949bbe
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-11-06 23:05:21 +0600

    Remove unnecessary lines

--- movies/models.py
+++ movies/models.py
@@ -30,12 +30,3 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        if self.upload_image:
-            super().save(*args, **kwargs)
-            img = Image.open(self.upload_image.path)
-            if img.height > 512 or img.width > 512:
-                output_size = (512, 512)
-                img.resize(output_size)
-                img.save(self.upload_image.path)
